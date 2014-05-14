c77bfac8848ab7aa125cbece9fa8952fc6668539
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-05-14 22:54:43 +0200

    Adds __exit and __init macros

--- hello-1.c
+++ hello-1.c
@@ -2,14 +2,16 @@
 // module
 #include <linux/module.h> // needed by all modules
 #include <linux/kernel.h> // needed for KERN_INFO
+#include <linux/init.h> // needed for __init and __exit macros
+
 MODULE_LICENSE("GPLv2");
-int hello_init(void) {
+static int __init hello_init(void) {
   printk(KERN_INFO "Hello World 1.\n");
   // non 0 return means init_module failed and module can't be loaded.
   return 0;
 }
 
-void hello_exit(void) {
+static void __exit hello_exit(void) {
   printk(KERN_INFO "Goodbye World 1.\n");
 }
 module_init(hello_init);
